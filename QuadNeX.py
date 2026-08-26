c336f9d3ea8eb991e7bdd673651d5418e7e28efb
Dieego03 <diego.alejandre51175@alumnos.udg.mx>
2026-08-26 12:59:09 -0600

    render template

--- QuadNeX.py
+++ QuadNeX.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 QuadNeXApp   = Flask(__name__)
 
 @QuadNeXApp.route('/')
 def home():
-    return '<h1>hello world</h1>'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     QuadNeXApp.run(port=5000, debug=True)
